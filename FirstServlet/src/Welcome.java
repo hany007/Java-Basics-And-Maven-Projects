b6c25e246d59a847deea9541a52625cf02858a72
hany007 <burhanjwd@gmail.com>
2016-12-01 23:13:51 +0530

    First Servlet
    Example of creating servlet in eclipse ide

---
+++ FirstServlet/src/Welcome.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public Welcome() {
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html><body>");
+		out.print("<h3>WELCOME TO SERVLET</h3>");
+		out.print("</body></html>");
+	
+		
+		
+		
+	}
+
+	
+	}
+
+
